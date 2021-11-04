d741280c3c49cd63e83e96f579b11ee54db6c31d
KilianMcG <91564236+KilianMcG@users.noreply.github.com>
2021-11-04 14:27:04 +0000

    Streamlined list functionality

---
+++ Course.java
@@ -0,0 +1,61 @@
+import org.joda.time.LocalDate;
+
+import java.util.ArrayList;
+
+public class Course {
+    private String cName;
+    private ArrayList<Module> modules;
+    private ArrayList<Student> enrolledStudents;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public Course(String name, LocalDate start, LocalDate end) {
+        cName = name;
+        startDate = start;
+        endDate = end;
+
+        enrolledStudents = new ArrayList<>();
+        modules = new ArrayList<>();
+    }
+
+    public void setName(String newName) {
+        cName = newName;
+    }
+
+    public String getName() {
+        return cName;
+    }
+
+    public void setStart(LocalDate newStart) {
+        startDate = newStart;
+    }
+
+    public LocalDate getStart() {
+        return startDate;
+    }
+
+    public void setEnd(LocalDate newEnd) {
+        endDate = newEnd;
+    }
+
+    public LocalDate getEnd() {
+        return endDate;
+    }
+
+    public void addStudent(Student newStudent) {
+        enrolledStudents.add(newStudent);
+    }
+
+    public ArrayList<Student> getStudents() {
+        return enrolledStudents;
+    }
+
+    public void addModule(Module newMod) {
+        modules.add(newMod);
+        newMod.addCourse(this);
+    }
+
+    public ArrayList<Module> getModule() {
+        return modules;
+    }
+}
---
+++ Module.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+
+public class Module {
+    private String mName;
+    private String mID;
+    private ArrayList<Student> students;
+    private ArrayList<Course> courses;
+
+    public Module(String name, String ID) {
+        mName = name;
+        mID = ID;
+
+        students = new ArrayList<>();
+        courses = new ArrayList<>();
+    }
+
+    public void setName(String newName) {
+        mName = newName;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setID(String newID) {
+        mID = newID;
+    }
+
+    public String getID() {
+        return mID;
+    }
+
+    public void addStudent(Student newStudent) {
+        students.add(newStudent);
+        newStudent.addModule(this);
+    }
+
+    public ArrayList<Student> getStudents(){
+        return students;
+    }
+
+    public void addCourse(Course newCourse) {
+        courses.add(newCourse);
+    }
+
+    public ArrayList<Course> getCourses(){
+        return courses;
+    }
+}
